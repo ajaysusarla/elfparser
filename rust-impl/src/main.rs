90738238b319849697a9904e4546e92915e20aa0
Parthasarathi Susarla <ajaysusarla@gmail.com>
2016-03-30 14:47:01 +1100

    [rust] Implement ELF parsing as a separate module.

--- rust-impl/src/main.rs
+++ rust-impl/src/main.rs
@@ -1,3 +1,17 @@
+ // This program is free software; you can redistribute it and/or
+ // modify it under the terms of the GNU Lesser General Public
+ // License as published by the Free Software Foundation; either
+ // version 2 of the License, or (at your option) any later version.
+
+ // This library is distributed in the hope that it will be useful,
+ // but WITHOUT ANY WARRANTY; without even the implied warranty of
+ // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ // Lesser General Public License for more details.
+
+ // You should have received a copy of the GNU Lesser General Public
+ // License along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+mod elf;
 extern crate getopts;
 use getopts::Options;
 use std::env;
@@ -10,6 +24,7 @@ fn print_usage(program: &str, opts: Options) {
 
 fn parse_elf(input: &str) {
     println!("=== ELF info for `{}` ===\n", input);
+    elf::elf_init();
 }
 
 fn main() {
